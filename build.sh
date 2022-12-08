bdad984b9662d635ec05beaacedf3aa20ecf1976
2018008068 <aza1200@naver.com>
2022-12-09 01:08:53 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 pip install --upgrade pip
+pip install psycopg2-binary
+
 poetry install
 pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
